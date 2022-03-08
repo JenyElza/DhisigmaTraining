1e8631a8e889d1124252fa58d5628beccde9d02d
JenyElza <ladyofpeniel@gmail.com>
2022-03-08 18:12:27 +0530

    Update api.js
    Api updated

--- API Activities/NODE JS/api.js
+++ API Activities/NODE JS/api.js
@@ -1,20 +1,20 @@
 
-// Load the HTTP module
+// Load the HTTP module. Created on 7th March 2022
 const http = require('http');
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
-// Create the HTTP server
+// Create the HTTP server. Created on 7th March 2022
 const server = http.createServer()
 
-// Instantiate Express, Body-Parser and Mysql
+// Instantiate Express, Body-Parser and Mysql. Created on 7th March 2022
 const express = require('express')
 const app = express()
 var bodyParser = require('body-parser')
 var mysql = require('mysql');
 
-// Creating connection to Mysql 
+// Creating connection to Mysql. Created on 7th March 2022 
 var con = mysql.createConnection({
     host: "localhost",
     user: "root",
@@ -22,7 +22,7 @@ var con = mysql.createConnection({
     database: "ecommerceweb"
 });
 
-//Connecting to the database
+//Connecting to the database. Created on 7th March 2022
 con.connect(function (err) {
     if (err) throw err;
     console.log("Connected!");
@@ -30,7 +30,7 @@ con.connect(function (err) {
 
 app.use(bodyParser.json())
 
-// To get details of all countries from the database
+// To get details of all countries from the database. Created by Jeny Biju on 8th March 2022
 app.get('/getcountries', (req, res) => {
     con.query("select id, txtCountryName from country", function (err, result, fields) {
         res.send(view = {
